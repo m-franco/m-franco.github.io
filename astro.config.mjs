9803948e10f59d5e14a4652850e03ea96e56ef1e
Matias Franco <matiasfranco86@gmail.com>
2024-05-09 02:36:34 -0300

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 export default defineConfig({
     site: 'https://m-franco.github.io',
